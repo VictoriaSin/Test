7b87a04ef2215912d6bc2a828585ab9f720a3069
VictoriaSin <sinitvik@gmail.com>
2023-08-05 12:05:28 +0300

    23423423423234

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
+<<<<<<< Updated upstream
     cout << "Ny privet";
     cout << "Ny privet";
+=======
+    cout << "Vsem privet";
+>>>>>>> Stashed changes
     return 0;
 }
 
