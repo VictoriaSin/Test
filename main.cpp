06cfceec647dad97a0dfc80e510989fe9d630479
VictoriaSin <sinitvik@gmail.com>
2023-08-05 12:15:33 +0300

    test

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< Updated upstream
-    cout << "Ny privet";
-    cout << "Ny privet";
-=======
     cout << "Vsem privet";
->>>>>>> Stashed changes
     return 0;
 }
 
